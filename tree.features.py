c21f4b4f477e9d54af33b24751033dca74f314ad
Florian <f.muellerklein@gmail.com>
2014-12-18 14:21:18 -0500

    Update tree.features.py
    working tree feature creation and logit training!!!

--- tree.features.py
+++ tree.features.py
@@ -3,13 +3,14 @@ import sklearn as sk
 import numpy as np
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import SGDClassifier
 
 train_loc = 'train.day.hour.csv'
 test_loc = 'test.day.hour.csv'
 
 #def load_train_data(path):
-train = pd.read_csv(train_loc, dtype = str)
+train = pd.read_csv(train_loc, dtype = str, nrows = 2000000)
 click = train['click']
 train = train.drop('click', 1)
 train = train.drop('id', 1)
@@ -29,11 +30,14 @@ def load_test_data(path):
 
 rf = RandomForestClassifier(n_estimators = 500, max_depth = 3, verbose = 1, random_state = 80085)
 rf.fit(train, click)
-embedded = rf.apply(train)
+train_rf = rf.apply(train)
 
 train = None
+
+encoder = OneHotEncoder()
+embedded = encoder.fit_transform(train_rf)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~ strong independent python script to do logit ~~~~~~~~~~~~~~~~~~~~~~~
 
 logitsgd = SGDClassifier(loss='log', n_iter = 10, alpha=.003, penalty = 'l2', n_jobs = -1, verbose = 1)
-logitmodel = logitsgd.fit(X = embedded, y = click)
+logitsgd.fit(X = embedded, y = click)
