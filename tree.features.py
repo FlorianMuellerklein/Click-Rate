4f16f182576b52ad9d97f8b483b75f0a57235830
Florian <f.muellerklein@gmail.com>
2014-12-19 18:46:56 -0500

    Update tree.features.py
    made script look legit, still having memory problems with OneHotEncoder

--- tree.features.py
+++ tree.features.py
@@ -2,42 +2,93 @@ import pandas as pd
 import sklearn as sk
 import numpy as np
 
+from csv import DictReader
+from scipy.sparse import vstack
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import SGDClassifier
 
+SEED = 80085
 train_loc = 'train.day.hour.csv'
 test_loc = 'test.day.hour.csv'
 
-#def load_train_data(path):
-train = pd.read_csv(train_loc, dtype = str, nrows = 2000000)
-click = train['click']
-train = train.drop('click', 1)
-train = train.drop('id', 1)
-for colname in list(train.columns.values):
-	train[colname] = pd.Categorical.from_array(train[colname]).codes
+# load and prepare training data
+def load_train_data(path):
+	train = pd.read_csv(path, dtype = str, nrows = 4000000, low_memory = False)
+	click = train['click'].astype(int)
+	train = train.drop('click', 1)
+	train = train.drop('id', 1)
+	for colname in list(train.columns.values):
+		train[colname] = pd.Categorical.from_array(train[colname]).codes
 	
-#return train, click
-	
+	return train, click
+
+# load and prepare testing data	
 def load_test_data(path):
-	test = pd.read_csv(path)
-	id = test['id']
+	test = pd.read_csv(path, dtype = str, low_memory = False)
 	test = test.drop('id', 1)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~ strong independent python script to load data ~~~~~~~~~~~~~~~~~~~~~~~
-
-#load_train_data(train_loc)
-
-rf = RandomForestClassifier(n_estimators = 500, max_depth = 3, verbose = 1, random_state = 80085)
-rf.fit(train, click)
-train_rf = rf.apply(train)
-
-train = None
-
-encoder = OneHotEncoder()
-embedded = encoder.fit_transform(train_rf)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~ strong independent python script to do logit ~~~~~~~~~~~~~~~~~~~~~~~
-
-logitsgd = SGDClassifier(loss='log', n_iter = 10, alpha=.003, penalty = 'l2', n_jobs = -1, verbose = 1)
-logitsgd.fit(X = embedded, y = click)
+	for colname in list(test.columns.values):
+		test[colname] = pd.Categorical.from_array(test[colname]).codes
+	
+	return test
+	
+def main():
+	# initialize sklearn objects
+	rf = RandomForestClassifier(n_estimators = 300, max_depth = 3, verbose = 1, random_state = SEED)
+	logitsgd = SGDClassifier(loss ='log', n_jobs = -1, verbose = 1)
+	encoder = OneHotEncoder()
+	
+	train, click = load_train_data(train_loc)
+	
+	# rf feature transformation
+	rf.fit(train, click)
+	train_rf = rf.apply(train)
+	train = None
+	
+	# encode rf features for logit
+	encoder.fit(train_rf)
+	print('transforming ...')
+	embed_one = encoder.transform(train_rf[0:1000000,])
+	embed_two = encoder.transform(train_rf[1000001:2000000,])
+	embed_three = encoder.transform(train_rf[2000001:3000000,])
+	embed_four = encoder.transform(train_rf[3000001:4000000,])
+	embedded = vstack((embed_one, embed_two, embed_three, embed_four))
+	embed_one = None
+	embed_two = None
+	embed_three = None
+	embed_four = None
+	train_rf = None
+	
+	# train model
+	logitsgd.fit(X = embedded, y = click)
+	embedded = None
+	
+	# load testing data
+	test = load_test_data(test_loc)
+	
+	# rf transform test
+	test_rf = rf.apply(test)
+	test = None
+	
+	# encode test
+	print('transforming ...')
+	embed_one = encoder.transform(test_rf[0:1000000,])
+	embed_two = encoder.transform(test_rf[1000001:2000000,])
+	embed_three = encoder.transform(test_rf[2000001:3000000,])
+	embed_four = encoder.transform(test_rf[3000001:4577464,])
+	embedded_test = vstack((embed_one, embed_two, embed_three, embed_four))
+	embed_one = None
+	embed_two = None
+	embed_three = None
+	embed_four = None
+	test_rf = None
+	
+	# make predictions
+	prediction = logitsgd.predict_proba(embedded_test)
+	
+	# save predictions
+	prediction = np.array(prediction)
+	np.savetxt("predictions.csv", prediction, delimiter = ",")
+	
+if __name__ == '__main__':
+	main()
